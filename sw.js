8ffe3463af0a377f055311eade1379d784bf2822
Cristian Perciun <c.perciun@custom.it>
2025-05-29 20:23:36 +0200

    Revert "f"
    This reverts commit 79180ded513a7d98fef04264943bd38985202282.

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed.');
-  self.skipWaiting();
-});
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
